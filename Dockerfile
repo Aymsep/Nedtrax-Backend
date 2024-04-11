b4f16abc7d5fab240afe3f3c4a7c0786df712bb4
Aymsep <71574687+Aymsep@users.noreply.github.com>
2024-04-11 21:02:44 +0000

    added postgresql image and pgadmin image

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ COPY . .
 ENV PORT 3000
 EXPOSE $PORT
 CMD ["npm","run","dev"] 
+
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const Sequelize = require('sequelize');
+require('dotenv').config()
+
+
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DB,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    {
+        host: process.env.DB_host,
+        dialect: 'postgres',
+    }
+
+)
+
+sequelize.authenticate()
+.then(()=>{
+    console.log('authenticated successfully')
+})
+.catch(err=>{
+    console.log('error connecting',err)
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello World! from ayman deply');
+    res.send('Hello World! from ayman www');
 });
 
 app.get('/users', (req, res) => {
